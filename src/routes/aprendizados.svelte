519d29afd5943de127b01bd5ee7f585bfb3aeb42
Otavio Corrêa <otavio@seguroviagem.srv.br>
2020-10-28 18:47:07 -0300

    Adicionada tela de aprendizados

---
+++ src/routes/aprendizados.svelte
@@ -0,0 +1,27 @@
+<script>
+    const list = (async () => {
+        const response = await fetch('https://api-diario-rails-otavio.herokuapp.com/aprendizados.json', 
+        {
+            method: 'GET',
+            headers: {'otavio': 'ok'}
+        })
+        return await response.json()
+    })()
+</script>
+
+<style>
+
+</style>
+
+<svelte:head>
+	<title>Diário de Aprendizados</title>
+</svelte:head>
+
+
+{#await list}
+	<p>...carregando lista de Aprendizados</p>
+{:then data}
+	{data}
+{:catch error}
+    {error}
+{/await}
